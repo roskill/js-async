1746bd5a4148c89bd1e4e4160bec6252a369e9c9
Justin Liu <justin.liu@myob.com>
2019-11-27 14:10:30 +1100

    Demonstrate Promise.all

--- app/src/pages/HomePage.jsx
+++ app/src/pages/HomePage.jsx
@@ -30,18 +30,31 @@ const HomePage = () => {
   const [weather, setWeather] = useState({});
 
   useEffect(() => {
-    const bhutanId = '1252634';
-    // const apiKey = '71f1013cc4a78d563d0eeaf9b93bfce8';
-    const apiKey = '';
-    const url =
-      `https://api.openweathermap.org/data/2.5/weather?id=${bhutanId}&appid=${apiKey}`;
+    // const bhutanId = '1252634';
+    const apiKey = '71f1013cc4a78d563d0eeaf9b93bfce8';
+    const locations = [
+      'los+angeles,us',
+      'san+francisco,us',
+      'lone+pine,us',
+      'mariposa,us'
+    ];
+
+    const urls = locations.map(location => `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${apiKey}`)
+
+    // const url =
+    //   `https://api.openweathermap.org/data/2.5/weather?id=${bhutanId}&appid=${apiKey}`;
   //   getData(url, data => {
   //   const dataObj = JSON.parse(data);
   //   console.log(JSON.stringify(dataObj, null, 2));
   //   setWeather(dataObj);
   // });
-  getDataPromise(url)
-    .then(response => setWeather(response))
+  // getDataPromise(url)
+  setWeather({});
+
+  Promise.all([getDataPromise(urls[0]), getDataPromise(urls[1]), getDataPromise(urls[2]), getDataPromise(urls[3])])
+    .then(responses => {
+      responses.map(response => console.log(response))
+    })
     .catch(status => console.log(status));
 }, []);
 
