c7077b7e81cc48f39319c0e175ea9c6cecf2e5ab
Justin Liu <justin.liu@myob.com>
2019-11-27 11:43:26 +1100

    Begin refactoring to use Promise

--- app/src/pages/HomePage.jsx
+++ app/src/pages/HomePage.jsx
@@ -1,10 +1,22 @@
 import React, { useEffect, useState } from 'react';
 
-const getData = (url, success) => {
-  let httpRequest = new XMLHttpRequest();
-  httpRequest.open('GET', url);
-  httpRequest.onload = () => success(httpRequest.responseText);
-  httpRequest.send();
+// const getData = (url, success) => {
+//   let httpRequest = new XMLHttpRequest();
+//   httpRequest.open('GET', url);
+//   httpRequest.onload = () => success(httpRequest.responseText);
+//   httpRequest.send();
+// };
+
+const getDataPromise = (url) => {
+  return new Promise((resolve, reject) => {
+    let httpRequest = new XMLHttpRequest();
+    httpRequest.open('GET', url);
+    httpRequest.onload = () => {
+      if (httpRequest.status === 200) resolve(httpRequest.responseText);
+      else reject(Error(httpRequest.status))
+    }
+    httpRequest.send();  
+  })
 };
 
 const HomePage = () => {
@@ -15,11 +27,13 @@ const HomePage = () => {
     const apiKey = '71f1013cc4a78d563d0eeaf9b93bfce8';
     const url =
       `https://api.openweathermap.org/data/2.5/weather?id=${bhutanId}&appid=${apiKey}`;
-    getData(url, data => {
-    const dataObj = JSON.parse(data);
-    console.log(JSON.stringify(dataObj, null, 2));
-    setWeather(dataObj);
-  });
+  //   getData(url, data => {
+  //   const dataObj = JSON.parse(data);
+  //   console.log(JSON.stringify(dataObj, null, 2));
+  //   setWeather(dataObj);
+  // });
+      setWeather({});
+      console.log(getDataPromise(url))
   }, []);
 
   return (
