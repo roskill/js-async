346488f85cccce18750d8dc408a81fc55f07f058
Justin Liu <justin.liu@myob.com>
2019-11-27 15:31:52 +1100

    Display weather for multiple locations

--- app/src/pages/HomePage.jsx
+++ app/src/pages/HomePage.jsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react';
 
+const kelvinToCelcius = temp => (temp - 273.15).toFixed(0);
+
 const getDataPromise = url => {
   return new Promise((resolve, reject) => {
     let httpRequest = new XMLHttpRequest();
@@ -20,7 +22,7 @@ const getDataPromise = url => {
 };
 
 const HomePage = () => {
-  const [weather, setWeather] = useState({});
+  const [weather, setWeather] = useState([]);
 
   useEffect(() => {
     const apiKey = '71f1013cc4a78d563d0eeaf9b93bfce8';
@@ -36,16 +38,15 @@ const HomePage = () => {
         `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${apiKey}`
     );
 
-    setWeather({});
-
     (async () => {
       try {
         let responses = [];
-        responses.push(await getDataPromise(urls[0]));
-        responses.push(await getDataPromise(urls[1]));
-        responses.push(await getDataPromise(urls[2]));
-        responses.push(await getDataPromise(urls[3]));
+        responses.push(JSON.parse(await getDataPromise(urls[0])));
+        responses.push(JSON.parse(await getDataPromise(urls[1])));
+        responses.push(JSON.parse(await getDataPromise(urls[2])));
+        responses.push(JSON.parse(await getDataPromise(urls[3])));
         responses.map(response => console.log(response));
+        setWeather(responses);
       } catch (status) {
         console.log(status);
       }
@@ -54,8 +55,17 @@ const HomePage = () => {
 
   return (
     <div>
-      <h2>Homepage</h2>
-      {JSON.stringify(weather)}
+      <h2>Weather</h2>
+      {weather.map((location, key) => (
+        <div key={key}>
+          <h4>{location.name}</h4>
+          <img
+            src={`http://openweathermap.org/img/wn/${location.weather[0].icon}@2x.png`}
+          />
+          <p>{`Temperature: ${kelvinToCelcius(location.main.temp)}°C`}</p>
+          <p>{`${location.weather[0].main}, ${location.weather[0].description}`}</p>
+        </div>
+      ))}
     </div>
   );
 };
